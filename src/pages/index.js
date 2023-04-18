2f4fe29aca40ddab53c6aa41b8c26ae505f4221b
Srinath-Laha <srinath.laha@brainiuminfotech.com>
2023-04-18 18:48:36 +0530

    dynamic page created done v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,46 @@
 
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
+// import Head from "next/head"
+// import styles from "../styles/Home.module.css"
  
-import { getStoryblokApi ,StoryblokComponent,useStoryblokState } from "@storyblok/react"
+// import { getStoryblokApi ,StoryblokComponent,useStoryblokState } from "@storyblok/react"
  
-export default function Home({ story }) {
-  story = useStoryblokState(story);
- console.log(story.content,'====')
-  return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// export default function Home({ story }) {
+//   story = useStoryblokState(story);
+//  console.log(story.content,'====')
+//   return (
+//     <div>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
  
-      <header>
-        <h1>{story ? story.name : "My Site"}</h1>
-      </header>
+//       <header>
+//         <h1>{story ? story.name : "My Site"}</h1>
+//       </header>
  
-      <StoryblokComponent blok={story.content} />
-    </div>
-  );
-}
+//       <StoryblokComponent blok={story.content} />
+//     </div>
+//   );
+// }
  
 
-////using getStaticProps
 
-// export async function getStaticProps() {
-//   // home is the default slug for the homepage in Storyblok
+
+// //using getServerSideProps
+ 
+//  export async function getServerSideProps(context) {
+//   // get the query object
+//   const insideStoryblok = context.query._storyblok;
+ 
 //   let slug = "home";
  
-//   // load the draft version
 //   let sbParams = {
-//     version: "draft", // or 'published'
+//     version: "published", // or 'draft'
 //   };
+ 
+//   if (insideStoryblok) {
+//     sbParams.version = "draft";
+//   }
  
 //   const storyblokApi = getStoryblokApi();
 //   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
@@ -43,26 +50,39 @@ export default function Home({ story }) {
 //       story: data ? data.story : false,
 //       key: data ? data.story.id : false,
 //     },
-//     revalidate: 3600, // revalidate every hour
 //   };
 // }
 
 
-//using getServerSideProps
+import Head from "next/head";
+import {
+  useStoryblokState,
+  getStoryblokApi,
+  StoryblokComponent,
+} from "@storyblok/react";
+import Layout from "../components/Layout";
  
- export async function getServerSideProps(context) {
-  // get the query object
-  const insideStoryblok = context.query._storyblok;
+export default function Home({ story }) {
+  story = useStoryblokState(story);
  
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <Layout>
+      <StoryblokComponent blok={story.content} />
+    </Layout>
+    </div>
+  );
+}
+ 
+export async function getStaticProps() {
   let slug = "home";
- 
   let sbParams = {
-    version: "published", // or 'draft'
+    version: "draft", // or 'published'
   };
- 
-  if (insideStoryblok) {
-    sbParams.version = "draft";
-  }
  
   const storyblokApi = getStoryblokApi();
   let { data } = await storyblokApi.get(`cdn/stories/${slug}`, sbParams);
@@ -72,8 +92,6 @@ export default function Home({ story }) {
       story: data ? data.story : false,
       key: data ? data.story.id : false,
     },
+    revalidate: 3600,
   };
 }
-
-
- 
